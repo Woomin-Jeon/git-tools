201dee0d050fc8f133d5a5e30bd40d16d4d8058a
Woomin-Jeon <dal96k@gmail.com>
2022-11-27 11:31:26 +0900

    fix: 일치하는 커맨드가 없는 경우에 대한 처리

--- bin/cli.js
+++ bin/cli.js
@@ -3,15 +3,26 @@
 import checkout from '../src/commands/checkout.js';
 import deleteBranch from '../src/commands/deleteBranch.js';
 
+const COMMAND_MAP = {
+  CHECKOUT: 'checkout',
+  DELETE_BRANCH: 'delete-branch',
+};
+
 const runner = async (params) => {
   const command = params[0];
 
-  if (command === 'checkout') {
-    checkout();
-  }
-
-  if (command === 'deleteBranch') {
-    deleteBranch();
+  switch (command) {
+    case COMMAND_MAP.CHECKOUT:
+      checkout();
+      break;
+    case COMMAND_MAP.DELETE_BRANCH:
+      deleteBranch();
+      break;
+    default:
+      console.log('There are no matching commands. Use the command below.');
+      Object.values(COMMAND_MAP).forEach((commandName) => {
+        console.log(`   $ git-tools ${commandName}`);
+      });
   }
 };
 
